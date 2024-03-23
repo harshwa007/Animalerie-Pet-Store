9fbbf0bbecabe9caae90cf2d9e31632da981d809
harshwa007 <harshwa007@gmail.com>
2024-03-23 22:03:08 +0530

    created controller and dtos

---
+++ Backend/Backend/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Context;
+using Backend.Dtos;
+using Backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //CRUD Functiionality
+        //create
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateUpdateProductDto dto)
+        {
+            var newProduct = new ProductEntity()
+            {
+                Brand = dto.Brand,
+                Title = dto.Title,
+            };
+            await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+            return Ok("Product Added Successfully");
+       
+        }
+        //Read
+        [HttpGet]
+        public async Task<ActionResult<List<ProductEntity>>> GetAllProduct()
+        {
+            var Products = await _context.Products.ToListAsync();
+            return Ok(Products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<ProductEntity>> GetProductByID([FromRoute] long id)
+        {
+
+        }
+    }
+}
---
+++ Backend/Backend/Dtos/CreateUpdateProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Dtos
+{
+    public class CreateUpdateProductDto
+    {
+        public String Brand { get; set; }
+        public String Title { get; set; }
+    }
+}
